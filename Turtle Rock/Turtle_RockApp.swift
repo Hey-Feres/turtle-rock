43ea102fc16c3a20aa0403cd17a6d7f11142cd1c
Bruno Feres <brunoferesvillela@icloud.com>
2023-10-31 15:36:51 -0300

    Initial Commit

---
+++ Turtle Rock/Turtle_RockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Turtle_RockApp.swift
+//  Turtle Rock
+//
+//  Created by Bruno Feres Villela on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Turtle_RockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
